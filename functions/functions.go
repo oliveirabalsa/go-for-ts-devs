5566979158baba2716ede8e567704db8c8436a60
Leonardo <oliveirabalsa2@gmail.com>
2023-06-30 18:53:02 -0300

    feat(function): ts and go examples

---
+++ functions/functions.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name  string
+	Age   int
+	Email string
+}
+
+func main() {
+	fmt.Println("Hello World")
+	withParams("Leonardo")
+	fmt.Println(oneReturnedValue("Leonardo"))
+	fmt.Println(twoReturnedValues("Leonardo"))
+	name, nameLength := twoReturnedValues("Leonardo")
+	fmt.Println(name, nameLength)
+	fmt.Println(withTypeReturn("Leonardo"))
+}
+
+func withParams(name string) {
+	fmt.Println("Hello", name)
+}
+
+func oneReturnedValue(name string) string {
+	return "Hello " + name
+}
+
+func twoReturnedValues(name string) (string, int) {
+	return name, len(name)
+}
+
+func withTypeReturn(name string) User {
+	return User{Name: name, Age: 50, Email: "oliveirabalsa2@gmail.com"}
+}
