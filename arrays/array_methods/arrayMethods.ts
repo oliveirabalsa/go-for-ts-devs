239d7cfd7120c305388857e8e9015673aafe1fa3
Leonardo <oliveirabalsa2@gmail.com>
2023-07-04 00:40:00 -0300

    feat(arrays): add arrays examples go and ts

---
+++ arrays/array_methods/arrayMethods.ts
@@ -0,0 +1,24 @@
+function main() {
+  // Array declaration and initialization
+  let numbers: number[] = [1, 2, 3, 4, 5];
+  console.log("Array:", numbers);
+
+  // Push an element
+  numbers.push(6);
+  console.log("Pushed Array:", numbers);
+
+  // Copy an array/slice
+  const copiedNumbers: number[] = numbers.slice();
+  console.log("Copied Array:", copiedNumbers);
+
+  // Slice an array/slice
+  const slicedNumbers: number[] = numbers.slice(1, 4); // [2, 3, 4]
+  console.log("Sliced Array:", slicedNumbers);
+
+  // Modify the slice
+  slicedNumbers[0] = 99;
+  console.log("Modified Slice:", slicedNumbers);
+  console.log("Original Array:", numbers);
+}
+
+main();
---
+++ arrays/array_methods/array_methods.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Array declaration and initialization
+	numbers := []int{1, 2, 3, 4, 5}
+	fmt.Println("Array:", numbers)
+
+	// Append an element
+	numbers = append(numbers, 6)
+	fmt.Println("Appended Array:", numbers)
+
+	// Copy an array/slice
+	copiedNumbers := make([]int, len(numbers))
+	copy(copiedNumbers, numbers)
+	fmt.Println("Copied Array:", copiedNumbers)
+
+	// Slice an array/slice
+	slicedNumbers := numbers[1:4] // [2, 3, 4]
+	fmt.Println("Sliced Array:", slicedNumbers)
+
+	// Modify the slice
+	// Modifying a sliced array in Go, reflect on original array, because a slice is just a copy of array references;
+	slicedNumbers[0] = 99
+	fmt.Println("Modified Slice:", slicedNumbers)
+	fmt.Println("Original Array:", numbers)
+}
